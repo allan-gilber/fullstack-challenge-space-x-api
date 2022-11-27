ecd3a98d47fbd24cb9cb90689f89fe22b11cdd0d
allan-gilber <allangilber@yahoo.com.br>
2022-11-27 01:05:58 -0300

    feat: created error message controller and its logic

---
+++ src/application/MessageErrorsController/MessageErrorsController.ts
@@ -0,0 +1,12 @@
+import errorMessageBusiness from '../../business/errorBusiness/ErrorMessageBusiness';
+import {errorMessage} from '../../models/errorMessageModels';
+
+/** This class is used to get the error message from the errorMessageBusiness class.
+ * @property {string} errorCode;
+ * @returns {string} errorMessage;
+*/
+export default class MessageErrorsController {
+  getErrorMessage(errorCode: any): errorMessage {
+    return new errorMessageBusiness().requestErrorMessage(errorCode);
+  }
+}
